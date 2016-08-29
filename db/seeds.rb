4aec56c8de125481e03f44140f3f2af88a630ce8
aaronelm <aaron@elmdigital.com>
2016-08-28 22:37:14 -0700

    Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "ak47ogcjm19", email: "aaron@elmdigital1.com", password: "everyday", password_confirmation: "everyday")
+User.create(username: "ak47ogcjm18", email: "aaron@elmdigital2.com", password: "everyday", password_confirmation: "everyday")
+User.create(username: "ak47ogcjm17", email: "aaron@elmdigital3.com", password: "everyday", password_confirmation: "everyday")
+User.create(username: "ak47ogcjm16", email: "aaron@elmdigital4.com", password: "everyday", password_confirmation: "everyday")
+User.create(username: "ak47ogcjm15", email: "aaron@elmdigital5.com", password: "everyday", password_confirmation: "everyday")
+p "TEST USERS CREATED"
