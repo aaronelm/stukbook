d190b427d409b12ee892fd7d7f42d31d80cce0d2
aaronelm <aaron@elmdigital.com>
2016-09-13 23:48:50 -0500

    Added posts controller, views and routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,11 @@ class Post < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :content
 
-  auto_html_for :content do
-    html_escape
-    image
-    youtube(sidth: "100%", height: 250, autoplay: false)
-    link target: "_blank", rel: "nofollow"
-    simple_format
+    auto_html_for :content do
+      html_escape
+      image
+      youtube(width: "100%", height: 250, autoplay: false)
+      link target: "_blank", rel: "nofollow"
+      simple_format
   end
 end
